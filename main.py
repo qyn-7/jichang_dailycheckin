857f2b3165af1dd3224d718507fae632ded21c40
qyn <106135858+qyn-7@users.noreply.github.com>
2025-07-30 12:29:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -28,9 +28,9 @@ def push(content):
 
 # 会不定时更新域名，记得Sync fork
 
-login_url = 'https://ikuuu.one/auth/login'
-check_url = 'https://ikuuu.one/user/checkin'
-info_url = 'https://ikuuu.one/user/profile'
+login_url = 'https://ikuuu.ch/auth/login'
+check_url = 'https://ikuuu.ch/user/checkin'
+info_url = 'https://ikuuu.ch/user/profile'
 
 header = {
         'origin': 'https://ikuuu.one',
