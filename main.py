8bc95eac935c6384e4fd012864a200cf8d9d1751
qyn <106135858+qyn-7@users.noreply.github.com>
2026-03-24 09:57:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -28,12 +28,12 @@ def push(content):
 
 # 会不定时更新域名，记得Sync fork
 
-login_url = 'https://ikuuu.fyi/auth/login'
-check_url = 'https://ikuuu.fyi/user/checkin'
-info_url = 'https://ikuuu.fyi/user/profile'
+login_url = 'https://ikuuu.nl/auth/login'
+check_url = 'https://ikuuu.nl/user/checkin'
+info_url = 'https://ikuuu.nl/user/profile'
 
 header = {
-        'origin': 'https://ikuuu.nl',
+        'origin': 'https://ikuuu.one',
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
 }
 
